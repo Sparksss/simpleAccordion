18dc154dc46f467082f7b775543bcc68cb0b9b8f
Ilya Rogatkin <toch.in.go@googlemail.com>
2018-03-03 22:11:19 +0300

    Use toggle for switch show/hide mode

--- js/Accordion.js
+++ js/Accordion.js
@@ -9,9 +9,6 @@ function showArticle(){
 list.addEventListener("click", function(event){
   if(event.target.name === "show"){
       let panel = event.target.nextElementSibling;
-      panel.classList.remove("hide");
-  }else if(event.target.name === "hide"){
-      let panel = event.target.parentElement;
-      panel.classList.add("hide");
+      panel.classList.toggle("hide");
   }
 });
